578ead36d2009f0ded994391f73fc93ea48cc9a1
KozhhekinAN <kozhekin67@gmail.com>
2025-07-24 22:07:02 +0300

    vik commit

--- api/server.js
+++ api/server.js
@@ -1,43 +1,32 @@
 const jsonServer = require('json-server');
-const fs = require('fs'); // Импортируем модуль fs
-const path = require('path');
 
 const server = jsonServer.create();
-const filePath = path.join(__dirname, 'db.json'); // Указываем путь к файлу db.json
 
-// Читаем данные из файла db.json
-const data = fs.readFileSync(filePath, "utf-8");
-const db = JSON.parse(data); // Парсим данные из JSON в объект
-
-// Создаем маршрутизатор на основе прочитанных данных
+// Uncomment to allow write operations
+const fs = require('fs');
+// const path = require('path')
+// const filePath = path.join('db.json')
+const data = fs.readFileSync(filePath, 'utf-8');
+const db = JSON.parse(data);
 const router = jsonServer.router(db);
+
+// Comment out to allow write operations
+//const router = jsonServer.router('db.json');
 
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-
-// Перезапись маршрутов
+// Add this before server.use(router)
 server.use(
   jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id',
   })
 );
-
-// Обработка POST, PUT и DELETE запросов для записи в файл
-server.use((req, res, next) => {
-  if (req.method === 'POST' || req.method === 'PUT' || req.method === 'DELETE') {
-    // После обработки запроса обновляем файл db.json
-    const newData = JSON.stringify(router.db.getState(), null, 2);
-    fs.writeFileSync(filePath, newData, 'utf-8');
-  }
-  next();
-});
-
 server.use(router);
 server.listen(3000, () => {
   console.log('JSON Server is running');
 });
 
-// Экспортируем сервер
+// Export the Server API
 module.exports = server;
